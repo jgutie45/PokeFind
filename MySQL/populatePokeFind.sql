cfd7eccc42f943f282b93c1f39c6fe47b9e5baca
jgutie45 <jgutie45@uic.edu>
2021-05-02 02:42:02 -0500

    Added Authentication and Connected to a Server

--- MySQL/populatePokeFind.sql
+++ MySQL/populatePokeFind.sql
@@ -1,6 +1,6 @@
 SET GLOBAL local_infile=1;
 USE PokeFind;
-LOAD DATA LOCAL INFILE 'C:/Temp/pokemon.csv' INTO TABLE pokemon FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE 'C:/Temp/attack.csv' INTO TABLE attack FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE 'C:/Temp/weakness.csv' INTO TABLE weakness FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE 'C:/Temp/price.csv' INTO TABLE price FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'C:\Users\yasmi\Desktop\CS480FinalProject\PokeFind\Scripts\pokemon.csv' INTO TABLE pokemon FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'C:\Users\yasmi\Desktop\CS480FinalProject\PokeFind\Scripts\attack.csv' INTO TABLE attack FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'C:\Users\yasmi\Desktop\CS480FinalProject\PokeFind\Scripts\weakness.csv' INTO TABLE weakness FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'C:\Users\yasmi\Desktop\CS480FinalProject\PokeFind\Scripts\price.csv' INTO TABLE price FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
