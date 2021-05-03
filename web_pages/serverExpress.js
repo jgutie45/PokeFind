7139969f5c14923dc98b60fa903006d069448cbe
jgutie45 <jgutie45@uic.edu>
2021-05-02 20:31:10 -0500

    Added Webpage Folder and Installed EJS

---
+++ web_pages/serverExpress.js
@@ -0,0 +1,32 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+var path = require('path')
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static(path.join(__dirname + '/web_pages'))); //('web_pages'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(8080);
+
+
+var runQuery = require('./runthis.js')
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/Home.html')) //.html'))
+})
+app.get('/js', function (req, res) {
+	var myVar = "Apples";
+	res.render(path.join(__dirname + '/testjs'), { //.html'))
+	myVar: myVar
+    }
+    )
+	})
+app.get('/db', async function (req, res) {
+	var result = await runQuery.buildPromise()
+	res.send(result)
+})
+
+app.listen(3000)
